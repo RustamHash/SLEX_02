4ea1c4599704a02d2452e26a06000eac61060633
rhashhanok <rhashhanok220@mail.ru>
2024-04-28 03:47:47 +0300

    add class Filial

--- base_app/admin.py
+++ base_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from base_app.models import Filial
 
-# Register your models here.
+
+class FilialAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+    class Meta:
+        model = Filial
+        fields = '__all__'
+
+
+admin.site.register(Filial, FilialAdmin)
--- base_app/models.py
+++ base_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse_lazy
 
-# Create your models here.
+
+class Filial(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    position = models.IntegerField(default=0)
+    as_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse_lazy('filial-detail', kwargs={'slug': self.slug})
+
+    def get_home_url(self):
+        return reverse_lazy(f'{self.slug}:home')
+
+    class Meta:
+        ordering = ('position',)
+        verbose_name = 'Филиал'
+        verbose_name_plural = 'Филиалы'
