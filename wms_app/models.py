cb68421429a6576cefa1816ed18edc60bf6b9c58
rhashhanok <rhashhanok220@mail.ru>
2024-04-29 15:56:38 +0300

    add wms_app delete krd_app or rnd_app

--- wms_app/models.py
+++ wms_app/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+import requests
 
-# Create your models here.
+
+class WMS:
+    def __init__(self, ):
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Connection': 'keep-alive'
+        }
+        self.server = '172.172.185.67'
+        self.infobase = 'krd_itc_wms'
+        self.username = 'ODUser'
+        self.password = 249981
+        self.params = None
+        self.full_url = f"http://{self.server}/{self.infobase}/odata/standard.odata/"
+        self._auth = requests.auth.HTTPBasicAuth(self.username, self.password)
+
+    def connect(self, _top=None):
+
+        if _top is not None:
+            if self.params is None:
+                self.params = f"$top={_top}"
+            else:
+                self.params = f"{self.params}&$top={_top}"
+        response = requests.get(url=self.full_url, headers=self.headers, auth=self._auth, params=self.params)
+        return response
